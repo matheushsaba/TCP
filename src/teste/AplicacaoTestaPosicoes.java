ff8dbd2aa9a8dfb290795f39a96094d48bc7eaef
matheushsaba <matheushs15@hotmail.com>
2022-12-28 16:39:45 -0300

    Inserçao de novas mensagens de impressao

--- src/teste/AplicacaoTestaPosicoes.java
+++ src/teste/AplicacaoTestaPosicoes.java
@@ -19,6 +19,7 @@ public class AplicacaoTestaPosicoes
         PosicaoMapa4x4 p1 = new PosicaoMapa4x4();
         ValidacaoInput.solicitarCoordenada(p1, ValidacaoInput.Eixo.x);
         ValidacaoInput.solicitarCoordenada(p1, ValidacaoInput.Eixo.y);
+        System.out.print("Coordenadas de p1: ");
         p1.imprime();
         
 
@@ -26,18 +27,21 @@ public class AplicacaoTestaPosicoes
         PosicaoMapa4x4 p2 = new PosicaoMapa4x4();
         ValidacaoInput.solicitarCoordenada(p2, ValidacaoInput.Eixo.x);
         ValidacaoInput.solicitarCoordenada(p2, ValidacaoInput.Eixo.y);
+        System.out.print("Coordenadas de p2: ");
         p2.imprime();
-        p1.distancia(p2);
+        System.out.println("Distancia entre p1 e p2: " + p1.distancia(p2));
 
 
         //Criaçao de p3
         PosicaoMapa4x4 p3 = p1.copy();
+        System.out.print("Coordenadas de p3: ");
+        p3.imprime();
 
 
         //Verificaçao se p1, p2 e p3 estao sobrepostos
-        boolean sobreposicaoP1P2 = PosicaoMapa4x4.estaoNaMesmaPosicao(p1, p2);
-        boolean sobreposicaoP2P3 = PosicaoMapa4x4.estaoNaMesmaPosicao(p2, p3);
-        boolean sobreposicaoP1P3 = PosicaoMapa4x4.estaoNaMesmaPosicao(p1, p3);
+        System.out.println("Ha sobreposiçao entre p1 e p2: " + PosicaoMapa4x4.estaoNaMesmaPosicao(p1, p2));
+        System.out.println("Ha sobreposiçao entre p2 e p3: " + PosicaoMapa4x4.estaoNaMesmaPosicao(p2, p3));
+        System.out.println("Ha sobreposiçao entre p1 e p3: " + PosicaoMapa4x4.estaoNaMesmaPosicao(p1, p3));
 
 
         //Criaçao da lista e impressao do mapa
@@ -52,26 +56,19 @@ public class AplicacaoTestaPosicoes
     {
         HashSet<String> coordenadasDosDrones = new HashSet<>();
 
-        for (PosicaoMapa4x4 ponto : pontos) 
+        for (PosicaoMapa4x4 ponto : pontos)             //Cria um hashset com as coordenadas dos drones em formato "x,y"
         {
             String coordenada = ponto.getX() + "," + ponto.getY();
             coordenadasDosDrones.add(coordenada);
         }
 
-        for (int i = 0; i < MAP_HEIGHT; i++)
+        for (int i = 0; i < MAP_HEIGHT; i++)            //Faz um loop na matriz de posiçoes e imprime o valor de cada uma
         {
             for (int j = 0; j < MAP_WIDTH; j++)
             {
                 String coordenada = i + "," + j;
-
-                if (coordenadasDosDrones.contains(coordenada))
-                {
-                    System.out.print("X ");
-                }
-                else
-                {
-                    System.out.print("- ");
-                }
+                String valorPosicao = (coordenadasDosDrones.contains(coordenada)) ? "X " : "- ";    //Se o hashset tiver alguma coordenada, imprime X, senao, imprime -
+                System.out.print(valorPosicao);
             }
             System.out.println();
         }
